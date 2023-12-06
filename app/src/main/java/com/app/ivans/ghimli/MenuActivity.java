d355bb65a6ac657958a1d49cff59f707fb72094d
Ivan Suhendra <38711246+van-grafity@users.noreply.github.com>
2023-12-06 11:15:51 +0700

    fixing hamburger cannot listener

--- app/src/main/java/com/app/ivans/ghimli/MenuActivity.java
+++ app/src/main/java/com/app/ivans/ghimli/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.app.ivans.ghimli;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
@@ -27,9 +30,11 @@ import com.app.ivans.ghimli.databinding.ActivityMenuBinding;
 import com.app.ivans.ghimli.net.API;
 import com.google.android.material.navigation.NavigationView;
 
+import de.hdodenhof.circleimageview.BuildConfig;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MenuActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "MenuActivity";
     private ActivityMenuBinding binding;
     private CircleImageView circleImageView;
 
@@ -49,48 +54,29 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar = binding.included.toolbar;
-        toolbar.setTitle("Home");
-        setSupportActionBar(toolbar);
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        drawer = binding.drawerLayout;
-        final ActionBar actionBar = getSupportActionBar();
-
-        if (actionBar != null) {
-
-            actionBar.setDisplayShowTitleEnabled(true);
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setDisplayHomeAsUpEnabled(true);
+        if (BuildConfig.DEBUG) {
+            Log.i(TAG, "onCreate: DEBUG");
         }
-        
+        drawer = binding.drawerLayout;
+        Toolbar toolbar = binding.included.toolbar;
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             @Override
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                view.bringToFront();
-                view.requestLayout();
-            }
-            
-            @Override
             public void onDrawerOpened(View drawerView) {
-                supportInvalidateOptionsMenu();
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
             }
         };
-        toggle.setDrawerIndicatorEnabled(true);
-        toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(drawer.isDrawerOpen(GravityCompat.START)){
-                    drawer.closeDrawer(GravityCompat.START);
-                }else {
-                    drawer.openDrawer(GravityCompat.START);
-                }
-            }
-        });
         drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
         binding.navView.setNavigationItemSelectedListener(this);
 
         View headerContainer = binding.navView.getHeaderView(0);
@@ -101,36 +87,21 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
         TextView userEmail = headerContainer.findViewById(R.id.emailOfUser);
         userEmail.setText(API.currentUser(MenuActivity.this).getEmail());
 
-        Fragment fragment = HomeFragment.newInstance();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frame_container, fragment);
-        ft.commit();
-        currentFragment = fragment;
+        if (savedInstanceState == null) {
+            Fragment fragment = HomeFragment.newInstance();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+            currentFragment = fragment;
+            binding.navView.setCheckedItem(R.id.nav_home);
+        }
 
-        // binding.navView.setCheckedItem(R.id.nav_home);
-        this.getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
-            @Override
-            public void onBackStackChanged() {
-                Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.frame_container);
-                if (currentFragment instanceof HomeFragment) {
-                    binding.navView.setCheckedItem(R.id.nav_home);
-                    getSupportActionBar().setTitle("Home");
-                } else if (currentFragment instanceof LayerFragment) {
-                    binding.navView.setCheckedItem(R.id.nav_layer);
-                    getSupportActionBar().setTitle("Layer");
-                } else if (currentFragment instanceof CutterFragment) {
-                    binding.navView.setCheckedItem(R.id.nav_cutter);
-                    getSupportActionBar().setTitle("Cutter");
-                } else if (currentFragment instanceof CutPieceStockFragment) {
-                    binding.navView.setCheckedItem(R.id.nav_cut_piece_stock);
-                    getSupportActionBar().setTitle("Cut Piece Stock");
-                }
-            }
-        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -142,15 +113,14 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         int id = menuItem.getItemId();
-        
+
         if (menuItem.isChecked()) {
             menuItem.setChecked(false);
         } else {
             menuItem.setChecked(true);
         }
-
         if (id == R.id.nav_home) {
-            clearStack();
+//            clearStack();
             getSupportActionBar().setTitle("Home");
             Fragment homeFragment = HomeFragment.newInstance();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -159,7 +129,6 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
             currentFragment = homeFragment;
             menuItem.setChecked(true);
         } else if (id == R.id.nav_layer) {
-            clearStack();
             getSupportActionBar().setTitle("Layer");
             Fragment layerFragment = LayerFragment.newInstance();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -168,7 +137,6 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
             currentFragment = layerFragment;
             menuItem.setChecked(true);
         } else if (id == R.id.nav_cutter) {
-            clearStack();
             getSupportActionBar().setTitle("Cutter");
             Fragment cutterFragment = CutterFragment.newInstance();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -176,9 +144,7 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
             ft.commit();
             currentFragment = cutterFragment;
             menuItem.setChecked(true);
-
         } else if (id == R.id.nav_cut_piece_stock) {
-            clearStack();
             getSupportActionBar().setTitle("Cut Piece Stock");
             Fragment cutPieceStockFragment = CutPieceStockFragment.newInstance();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -186,9 +152,7 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
             ft.commit();
             currentFragment = cutPieceStockFragment;
             menuItem.setChecked(true);
-
         } else if (id == R.id.nav_logout) {
-
             AlertDialog alertDialog = new AlertDialog.Builder(this)
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .setMessage(R.string.want_to_logout)
@@ -200,11 +164,9 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
                     })
                     .setNegativeButton(R.string.cancel, null)
                     .show();
-
             alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(this, R.color.grey_dark));
             alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(this, R.color.grey_dark));
         }
-
         binding.navView.setCheckedItem(id);
         drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -239,11 +201,16 @@ public class MenuActivity extends BaseActivity implements NavigationView.OnNavig
         }
     }
 
-    // onPostCreate
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
     }
 
     private void closeApplication() {
